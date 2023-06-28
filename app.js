ac8a6df1c183cf290432de0b7f20406e81f69c1d
Roy <weichai.royston.yeo@stengg.com>
2023-06-28 11:13:07 +0800

    Basic jobbee project initial setup

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded( { extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`server listening to ${port}`)
-})
-
+  console.log(`server listening to ${port}`);
+});
