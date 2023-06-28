3fc879e186120c356a20f0d20904c319ee08905b
Roy <weichai.royston.yeo@stengg.com>
2023-06-28 10:36:05 +0800

    all about headers

--- dataResponse.js
+++ dataResponse.js
@@ -1,24 +1,30 @@
-const http = require('http')
-const port = process.env.PORT || 3000
+const http = require("http");
+const port = process.env.PORT || 3000;
 
-const data = [{
+const data = [
+  {
     id: 1,
     name: "Roy",
-
-},{
+  },
+  {
     id: 2,
     name: "Yin",
-
-},{
+  },
+  {
     id: 3,
     name: "Chin",
-
-},]
+  },
+];
 
 const server = http.createServer((req, res) => {
-    res.end(JSON.stringify({success : 'true', data}))
-})
+  // set the headers
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Lanaguage", "en-US");
+  res.setHeader("Date", new Date(Date.now()));
+  res.setHeader("X-Powered-By", "Node-js");
+  res.end(JSON.stringify({ success: true, results: data.length, data }));
+});
 
-server.listen(port, ()=> {
-    console.log('Server is started')
-})
+server.listen(port, () => {
+  console.log("Server is started");
+});
